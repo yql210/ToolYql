517cc3be6c5db99def4bb36afca540aab80e4377
yuanql <56667999+yql210@users.noreply.github.com>
2023-11-04 09:03:23 +0800

    数据集下载

--- Tool/DownloadDatasetHuggingface.py
+++ Tool/DownloadDatasetHuggingface.py
@@ -1,17 +1,30 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(repo_id="shibing624/sharegpt_gpt4", repo_type="dataset")
+# snapshot_download(repo_id="shibing624/sharegpt_gpt4", repo_type="dataset")
+# snapshot_download(repo_id="anon8231489123/ShareGPT_Vicuna_unfiltered", repo_type="dataset")
+# snapshot_download(repo_id="FreedomIntelligence/HuatuoGPT-sft-data-v1", repo_type="dataset")
 
-snapshot_download(repo_id="anon8231489123/ShareGPT_Vicuna_unfiltered", repo_type="dataset")
+snapshot_download(repo_id="shibing624/medical", repo_type="dataset")
+snapshot_download(repo_id="Linly-AI/Chinese-pretraining-dataset", repo_type="dataset")
+snapshot_download(repo_id="pleisto/wikipedia-cn-20230720-filtered", repo_type="dataset")
+snapshot_download(repo_id="BelleGroup/train_0.5M_CN", repo_type="dataset")
+snapshot_download(repo_id="BelleGroup/train_1M_CN", repo_type="dataset")
+snapshot_download(repo_id="shibing624/alpaca-zh", repo_type="dataset")
+snapshot_download(repo_id="Chinese-Vicuna/guanaco_belle_merge_v1.0", repo_type="dataset")
+snapshot_download(repo_id="RyokoAI/ShareGPT52K", repo_type="dataset")
+snapshot_download(repo_id="BelleGroup/multiturn_chat_0.8M", repo_type="dataset")
+snapshot_download(repo_id="fnlp/moss-002-sft-data", repo_type="dataset")
+snapshot_download(repo_id="FreedomIntelligence/ShareGPT-CN", repo_type="dataset")
 
-snapshot_download(repo_id="FreedomIntelligence/HuatuoGPT-sft-data-v1", repo_type="dataset")
 
-# snapshot_download(repo_id="shibing624/medical", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
-# snapshot_download(repo_id="", repo_type="dataset")
+snapshot_download(repo_id="OpenAssistant/oasst1", repo_type="dataset")
+snapshot_download(repo_id="tasksource/oasst1_pairwise_rlhf_reward", repo_type="dataset")
+snapshot_download(repo_id="Dahoas/full-hh-rlhf", repo_type="dataset")
+snapshot_download(repo_id="Dahoas/static-hh", repo_type="dataset")
+snapshot_download(repo_id="Dahoas/rm-static", repo_type="dataset")
+snapshot_download(repo_id="liswei/rm-static-zhTW", repo_type="dataset")
+snapshot_download(repo_id="yitingxie/rlhf-reward-datasets", repo_type="dataset")
+snapshot_download(repo_id="liyucheng/zhihu_rlhf_3k", repo_type="dataset")
+
+
 # snapshot_download(repo_id="", repo_type="dataset")
