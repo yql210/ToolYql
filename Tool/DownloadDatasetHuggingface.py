16fd3e28fb1847292c1508d82f30cc2bfe525d37
yql210 <56667999+yql210@users.noreply.github.com>
2023-11-04 08:53:45 +0800

    huggingface模型、数据集下载

---
+++ Tool/DownloadDatasetHuggingface.py
@@ -0,0 +1,17 @@
+from huggingface_hub import snapshot_download
+
+snapshot_download(repo_id="shibing624/sharegpt_gpt4", repo_type="dataset")
+
+snapshot_download(repo_id="anon8231489123/ShareGPT_Vicuna_unfiltered", repo_type="dataset")
+
+snapshot_download(repo_id="FreedomIntelligence/HuatuoGPT-sft-data-v1", repo_type="dataset")
+
+# snapshot_download(repo_id="shibing624/medical", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
+# snapshot_download(repo_id="", repo_type="dataset")
---
+++ Tool/DownloadModelHuggingface.py
@@ -0,0 +1,15 @@
+from huggingface_hub import snapshot_download
+# snapshot_download(repo_id="THUDM/chatglm3-6b", local_dir="E:\\a_search\\github\\download\\THUDM\\chatglm3-6b")
+
+
+snapshot_download(repo_id="HuggingFaceH4/zephyr-7b-beta")
+
+
+# snapshot_download(repo_id="xverse/XVERSE-13B-Chat")
+# snapshot_download(repo_id="mistralai/Mistral-7B-v0.1")
+# snapshot_download(repo_id="hfl/chinese-alpaca-2-7b")
+# snapshot_download(repo_id="meta-llama/Llama-2-7b-chat-hf")
+# snapshot_download(repo_id="daryl149/llama-2-7b-chat-hf")
+
+
+# snapshot_download(repo_id="")
