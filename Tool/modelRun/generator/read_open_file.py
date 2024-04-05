df459d8fa65fc451400fe3a2f663a058202d5bd9
yuanql <56667999+yql210@users.noreply.github.com>
2024-04-05 12:24:36 +0800

    筛选数据集Belle_open_source_0.5M
    筛选数据集Belle_open_source_0.5M，将所有包换主题的问句筛选出来，筛选出来了2.5w条

---
+++ Tool/modelRun/generator/read_open_file.py
@@ -0,0 +1,107 @@
+import json
+import sys
+import jsonlines
+from tqdm import tqdm
+
+
+def read_large_json(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            yield json.loads(line)
+
+
+def store_json_per_line(json_data, output_file):
+    with open(output_file, "a", encoding="utf-8") as file:
+        for item in json_data:
+            # json.dump(item, file, ensure_ascii=False, indent=4)
+            file.write(item + "\n")
+
+
+def load_jsonlines(file):
+    with jsonlines.open(file, 'r') as jsonl_f:
+        lst = [obj for obj in jsonl_f]
+    return lst
+
+
+def load_json(fname, mode="r", encoding="utf8"):
+    if "b" in mode:
+        encoding = None
+    with open(fname, mode=mode, encoding=encoding) as f:
+        return json.load(f)
+
+
+def load_file(file_name):
+    print(file_name)
+    if file_name.endswith(".json"):
+        data = json.load(open(file_name, encoding="utf-8"))
+    elif file_name.endswith(".jsonl"):
+        data = load_jsonlines(file_name)
+    else:
+        if ".json_" in file_name:
+            data = json.load(open(file_name))
+        elif ".jsonl_" in file_name:
+            data = load_jsonlines(file_name)
+    return data
+
+
+def load_all_files(file_paths):
+    final_results = {}
+    # for fp in file_paths:
+    data = load_file(file_paths)
+    for item in data:
+        q_id = item["id"] if "id" in item else item["q_id"]
+        final_results.setdefault(q_id, [])
+        final_results[q_id].append(item)
+    return final_results
+
+
+def main():
+    data_open_file = './data_open/Belle_open_source_0.5M.jsonl'
+
+    data_open_save_file = './data_open/Belle_open_source_主题.jsonl'
+
+    file_save_path = data_open_save_file
+
+    datas = load_file(data_open_file)
+
+    num_all = 0;
+    index = 0
+    index1 = 0
+    id_index = 0
+    items = []
+
+    for data in datas:
+        if "主题" in data['instruction']:
+            num_all += 1
+            # print(data)
+            data = json.dumps(data, ensure_ascii=False)
+            items.append(data)
+            # print(conversation)
+            index += 1
+
+            if index == 500:
+                store_json_per_line(items, file_save_path)
+                index = 0
+                items = []
+                # with open(file_output, 'a', encoding='utf-8') as file:
+                #     # 将标准输出重定向到文件
+                #     sys.stdout = file
+                #     print("--------------" + str(index1) + "----------------")
+
+
+    store_json_per_line(items, file_save_path)
+
+    print("\n----------end-------------------")
+    print(num_all)
+
+
+
+
+if __name__ == "__main__":
+    print("\nstart:\n\n")
+    main()
+    print("\n\nend!!!\n\n")
+
+
+
+
