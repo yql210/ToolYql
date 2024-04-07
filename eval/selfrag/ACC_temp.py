004c38d32fd39fa4ecf3f226102242fd92ea1ffd
yuanql <56667999+yql210@users.noreply.github.com>
2024-04-07 15:55:36 +0800

    模板性文件

---
+++ eval/selfrag/ACC_temp.py
@@ -0,0 +1,86 @@
+import json
+import jsonlines
+
+
+def read_large_json(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            yield json.loads(line)
+
+
+def store_json_per_line(json_data, output_file):
+    with open(output_file, "a", encoding="utf-8") as file:
+        for item in json_data:
+            # json.dump(item, file, ensure_ascii=False, indent=4)
+            file.write(item + "\n")
+
+
+def load_jsonlines(file):
+    with jsonlines.open(file, 'r') as jsonl_f:
+        lst = [obj for obj in jsonl_f]
+    return lst
+
+
+def load_json(fname, mode="r", encoding="utf8"):
+    if "b" in mode:
+        encoding = None
+    with open(fname, mode=mode, encoding=encoding) as f:
+        return json.load(f)
+
+def load_file(file_name):
+    print(file_name)
+    if file_name.endswith(".json"):
+        data = json.load(open(file_name, encoding="utf-8"))
+    elif file_name.endswith(".jsonl"):
+        data = load_jsonlines(file_name)
+    else:
+        if ".json_" in file_name:
+            data = json.load(open(file_name))
+        elif ".jsonl_" in file_name:
+            data = load_jsonlines(file_name)
+    return data
+
+
+def load_all_files(file_paths):
+    final_results = {}
+    # for fp in file_paths:
+    data = load_file(file_paths)
+    for item in data:
+        q_id = item["id"] if "id" in item else item["q_id"]
+        final_results.setdefault(q_id, [])
+        final_results[q_id].append(item)
+    return final_results
+
+def main():
+    file_output = './FINAL_OUTPUT_PATH/output.txt'
+    file_output_toLong = './FINAL_OUTPUT_PATH/output_toLong.txt'
+    file_output_error = './FINAL_OUTPUT_PATH/output_error.txt'
+
+    # with open(file_output, 'a', encoding='utf-8') as file:
+    #     # 将标准输出重定向到文件
+    #     sys.stdout = file
+    #     print("----------start-------------------")
+    #
+    # # 恢复标准输出
+    # sys.stdout = sys.__stdout__
+    # print("----------start-------------------")
+
+    file_path = './sa-self-gen-data/0405_sa_rag_1.3b_epcoh_20.jsonl'
+
+    input_datas = load_file(file_path)
+    for data in input_datas:
+        print(data)
+
+        print('end')
+
+
+
+if __name__ == "__main__":
+    print("\nstart:\n\n")
+    main()
+    print("\n\nend!!!\n\n")
+
+
+
+
+
